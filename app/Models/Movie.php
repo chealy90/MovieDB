6937a994e70baa771f8f19fc685a196a777d3257
shane-smyth <158466904+shane-smyth@users.noreply.github.com>
2025-04-07 09:06:37 +0100

    created movie migration and model

---
+++ app/Models/Movie.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'tmdb_id',
+        'title',
+        'release_year',
+        'poster_path',
+        'genre_ids',
+    ];
+
+    protected $casts = [
+        'genre_ids' => 'array',
+    ];
+}
---
+++ database/migrations/2025_04_03_203123_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tmdb_id')->unique();
+            $table->string('title');
+            $table->integer('release_year')->nullable();
+            $table->json('genre_ids')->nullable();
+            $table->string('poster_path');
+            $table->timestamps();
+
+            $table->index('tmdb_id');
+            $table->index('title');
+            $table->index('poster_path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
